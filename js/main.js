d1a69fd29b47ca6b5530e3b9101c3f4382d7f3ed
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-04 15:08:46 +0200

    feat: main js with console log done

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
+// creo variabile prezzo finale
+let finalPrice;
+
 // richiesta km da percorrere
 let trip = parseInt(prompt("Quanti sono i km da percorrere per il viaggio?"));
-console.log("Km da percorrere",trip);
+console.log("Km da percorrere", trip);
 
 // richiesta età del passeggero
 let age = parseInt(prompt("Quanti anni ha il passeggero?"));
-console.log("Anni del passeggero",age);
+console.log("Anni del passeggero", age);
 
 // calcolare prezzo totale del viaggio (0,21cents per km)
+const price = 0.21;
+console.log("Prezzo base per km", price);
 
+let noDiscount = (trip*price);
+console.log("Prezzo senza sconto", noDiscount);
 
-// va applicato sconto per gli under 18 di 20% sul prezzo finale
-
-// va applicato sconto per gli over 65 di 20% sul prezzo finale
+// va applicato sconto per gli under 18 di 20%, oppure per gli over 65 di 40% sul prezzo finale
+if (age < 18) {
+    finalPrice = (noDiscount-(noDiscount*0.2));
+} else if (age >= 65) {
+    finalPrice = (noDiscount-(noDiscount*0.4));
+} else {
+    finalPrice = noDiscount;
+}
 
 // prezzo finale dopo sconto
+console.log("Il prezzo del biglietto è di: ", finalPrice);
