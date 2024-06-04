af77178e179049dbd77425f8cb2a2bcaac4eb225
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-04 14:44:04 +0200

    feat: add logic steps on main js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// richiesta km da percorrere
+
+// richiesta età del passegero
+
+// calcolare prezzo totale del viaggio (0,21cents per km)
+
+// va applicato sconto per gli under 18 di 20% sul prezzo finale
+
+// va applicato sconto per gli over 65 di 20% sul prezzo finale
