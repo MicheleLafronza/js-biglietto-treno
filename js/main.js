ee0b9a60575457d92d4abd4581e9e635fd787b3c
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-04 15:40:42 +0200

    Esercizio completato

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // creo variabile prezzo finale
 let finalPrice;
+let message
 
 // richiesta km da percorrere
 let trip = parseInt(prompt("Quanti sono i km da percorrere per il viaggio?"));
@@ -19,11 +20,26 @@ console.log("Prezzo senza sconto", noDiscount);
 // va applicato sconto per gli under 18 di 20%, oppure per gli over 65 di 40% sul prezzo finale
 if (age < 18) {
     finalPrice = (noDiscount-(noDiscount*0.2));
+    message = ("E' stato applicato uno sconto del 20%");
 } else if (age >= 65) {
     finalPrice = (noDiscount-(noDiscount*0.4));
+    message = ("E' stato applicato uno sconto del 40%");
 } else {
     finalPrice = noDiscount;
+    message = ("Nessuno sconto è stato applicato");
 }
+console.log("Prezzo prima dell'arrotondamento: ", finalPrice);
 
-// prezzo finale dopo sconto
+// arrotondamento prezzo finale dopo sconto
+finalPrice = Math.round(finalPrice * 100) / 100;
 console.log("Il prezzo del biglietto è di: ", finalPrice);
+console.log(message);
+
+// mostro dati sulla pagina
+document.getElementById("trip").innerHTML ="I km da percorrere sono: " + trip;
+document.getElementById("age").innerHTML ="Anni del passeggero: " + age;
+document.getElementById("price").innerHTML ="Costo del biglietto per km: " + price;
+document.getElementById("final-price").innerHTML ="Prezzo totale del biglietto: " + finalPrice;
+document.getElementById("message").innerHTML = message;
+
+
