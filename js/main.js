54a8f0ffc55e9e9a323f68b2f9366a9a09c6cb7d
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-04 14:50:15 +0200

    feat: add age and trip variables

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 // richiesta km da percorrere
+let trip = parseInt(prompt("Quanti sono i km da percorrere per il viaggio?"));
+console.log("Km da percorrere",trip);
 
-// richiesta età del passegero
+// richiesta età del passeggero
+let age = parseInt(prompt("Quanti anni ha il passeggero?"));
+console.log("Anni del passeggero",age);
 
 // calcolare prezzo totale del viaggio (0,21cents per km)
+
 
 // va applicato sconto per gli under 18 di 20% sul prezzo finale
 
 // va applicato sconto per gli over 65 di 20% sul prezzo finale
+
+// prezzo finale dopo sconto
